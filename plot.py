3d0e702447aad0966494dcff545c17dd70441228
amtorresn <am.torresn@uniandes.edu.co>
2019-05-07 12:49:40 +0000

    1

--- plot.py
+++ plot.py
@@ -4,6 +4,22 @@ from mpl_toolkits.mplot3d import Axes3D
 
 data = np.loadtxt("datos.dat")
 
+fig = plt.figure(figsize = (15,7))
 
+plt.subplot(1,2,1)
+
+x = np.arange(0,1,0.01)
+y = np.arange(0,1,0.01)
+# ax = Axes3D(fig)
+# ax.plot_trisurf(x,y, data)
+
+plt.plot(x, data[0:100,0]/100)
+
+
+plt.subplot(1,2,2)
+plt.plot(x, data[0:100,0])
+plt.plot(x, data[0:100,1])
+plt.xlim(0,1)
+plt.ylim(-1,1)
 
 plt.savefig("fig.png")
--- sol.cpp
+++ sol.cpp
@@ -13,7 +13,7 @@ void solve(string filename);
 
 int main(){
     
-    solve("datos.dat")
+    solve("datos.dat");
     return 0;
 }
 
@@ -22,7 +22,40 @@ void solve(string filename){
     ofstream outfile;
     outfile.open(filename);
     
+    float dx = 0.01;
+    float dt = 0.01;
+    int Tmax = 6;
+    float c = 0.5;
+    float cc = dx/dt;
+    float c1 = c*c/(cc*cc);
+    float L = 1.0;
     
+    float xi[100][3];
+    float xii[100][3];
+    
+    for (int i = 0; i < 101; i++){
+        xi[i][0] = sin(i*M_PI/100);
+        xi[i][1] = 0; //not sure
+        xii[i][0] = xi[i][0];
+        xii[i][1] = xi[i][1];
+    }
+    
+    float t = 0;
+    while(t<=Tmax){
+        
+        for(int i = 1; i < 100; i++){
+            xi[i][1] = xi[i][0] + 0.5*c1*(xi[i+1][0] + xi[i-1][0] - 2*xi[i][0] );
+            xi[i][2] = 2*xi[i][1] - xi[i][0] + c1*(xi[i+1][1] + xi[i-1][1] - 2*xi[i][1] );
+        }
+        t = t+dt;
+        for(int i = 0; i < 101; i++){
+            xi[i][0] = xi[i][1];
+            xi[i][1] = xi[i][2];
+        }
+    }
+    for(int i = 0; i < 101; i++){
+            outfile << xii[i][0] << " " << xii[i][1] << " " << xi[i][0] <<  " " <<  xi[i][1] << " " << xii[i][2] << endl;
+     }
   
     outfile.close();
     
